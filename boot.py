855d0e2e92e40ca877b226ac8dda539d980946ce
evisvasiu <45014223+evisvasiu@users.noreply.github.com>
2022-02-20 00:38:03 +0100

    Update boot.py
    Added UV index sensor

--- boot.py
+++ boot.py
@@ -2,7 +2,7 @@
 import machine, onewire, ds18x20, time
 from mqttsimple import MQTTClient
 from time import sleep
-from machine import Pin
+from machine import Pin, ADC
 import dht
 import micropython
 import network
@@ -25,8 +25,8 @@ def deep_sleep(msecs):
 
 led = Pin(2, Pin.OUT)
 
-ssid = 'Pixel5'
-password = 'tir***021'
+ssid = 'Kabe*B6C'
+password = '802*562743'
 
 station = network.WLAN(network.STA_IF)
 ap_if = network.WLAN(network.AP_IF)
@@ -35,17 +35,17 @@ ap_if.active(False)
 station.active(True)
 station.connect(ssid, password)
 
-while station.isconnected() == False:  #The time it is still disconected to network
-  for i in range(100):                 #Led pulsing while is disconected
+while station.isconnected() == False:         #The time it is still disconected to network
+  for i in range(100):                       #Led pulsing while is disconected
     led.value(0)
     time.sleep(0.1)
     led.value(1)
     time.sleep(0.1)
-    if station.isconnected() == True:   #if it connects to wifi, loop closes, and the execution can go on out of this while loop
+    if station.isconnected() == True:         #if it connects to wifi, loop closes, and the execution can go on out of this while loop
       break
-    if i == 60:                         #if it does not connect in 60 loop times, it will going to deep sleep.
+    if i == 60:                               #if it does not connect in 60 loop times, it will going to deep sleep.
       print('Could not connect to network. Going to sleep')
-      deep_sleep(10000)                 #Going to deep sleep for 10 seconds.
+      #deep_sleep(sleep_time)                 #Going to deep sleep for a specified amount of time
     
 print('Connection successful')
 print(station.ifconfig())
@@ -54,18 +54,21 @@ print(station.ifconfig())
 "umqttsimple" library is installed from here: 
 https://raw.githubusercontent.com/RuiSantosdotme/ESP-MicroPython/master/code/MQTT/umqttsimple.py
 """
-ds_pin = machine.Pin(14)
+uv = ADC(0)                 #UV sensor connected to analog pin 0
+dht22 = dht.DHT22(Pin(14))  #DHT22 connected to pin GPIO14
+ds_pin = machine.Pin(5)     #DS18 connected to pin GPIO5
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 
 roms = ds_sensor.scan()
 print('Found DS devices: ', roms)
 
-mqtt_server = '13*46.220'
-user = 'je***ca'
-passw = '*'
+mqtt_server = '13*220'
+user = 'j*ca'
+passw = 'P**2'
 client_id = 'esp8266'
-topic_t = 'esp8266_temp'
-topic_h = 'esp8266_humi'
+topic_uv = 'uv'
+topic_t = 'dht22_temp'
+topic_h = 'dht22_humi'
 topic_sub = 'sleep'
 topic_ds = 'ds18'
 
@@ -85,7 +88,7 @@ def connect_and_subscribe():
 
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
-  time.sleep(20)
+  sleep(20)
   machine.reset()
 
 try:
@@ -96,21 +99,34 @@ except OSError as e:
 while True:
   try:
     led.value(1)
-    ds_sensor.convert_temp()
+    uv_volt = (uv.read()*3.3)/1023
+    dht22.measure()             #dht22 read function
+    temp = dht22.temperature()  #reading dht22 temp
+    humi = dht22.humidity()     #reading dht22 humidity
+    ds_sensor.convert_temp()    #reading ds18 temp
     sleep(10)
+    print('UV Index mV: %3.0f' %uv_volt)
+    print('Temperature: %3.1f C' %temp)
+    print('Humidity: %3.1f %%' %humi)
+    msg_uv = b'%3.0f' %uv_volt
+    msg_temp = b'%3.0f' %temp   #saving dht22 value
+    msg_humi = b'%3.0f' %humi   #saving dht22 value
     for rom in roms:
-      print(rom)
+      #print(rom)
       print(ds_sensor.read_temp(rom))
-    msg_ds = b'%3.0f' %ds_sensor.read_temp(rom)
-    client.publish(topic_ds, msg_ds)
+    msg_ds = b'%3.1f' %ds_sensor.read_temp(rom) #saving ds18 value
+    client.publish(topic_uv, msg_uv)          #publish uv index
+    client.publish(topic_t, msg_temp)           #publish dht temp
+    client.publish(topic_h, msg_humi)           #publish dht humidity
+    client.publish(topic_ds, msg_ds)            #publish ds18 temp
     led.value(0)
-    sleep(1)
-    print('Going to deep sleep')
-    #deep sleep command
-    print(sleep_time)
-    deep_sleep(sleep_time)
+    sleep(2)
+    #print('Going to deep sleep')                #deep sleep command
+    #print(sleep_time)
+    #deep_sleep(sleep_time)
     
   except OSError as e:
     print('Failed to read sensor. Reconnecing...')
     restart_and_reconnect()
+
 
